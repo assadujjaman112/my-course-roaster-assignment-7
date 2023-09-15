190b6e11d9972a4946b06fac222e49429e729712
assadujjaman112 <assadujjamanmurad120@gmail.com>
2023-09-15 23:08:58 +0600

    completed the selected course part with toast

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -1,13 +1,33 @@
 
+import PropTypes from 'prop-types';
+const Cart = ({ selectedCourse, totalPrice,totalCredit,creditRemaining }) => {
+  console.log(selectedCourse);
+  return (
+    <div className="bg-white rounded-lg p-4">
+      <h2 className="text-[#2F80ED] font-bold mb-4 ">
+        Credit Hour Remaining :
+      </h2>
+      <hr className="w-4/5 mx-auto" />
+      <h2 className="my-4 font-bold text-xl ">Course Name</h2>
+      {selectedCourse.map((course) => (
+        <li key={course.id}>{course.name}</li>
+      ))}
+      <hr className="w-4/5 mx-auto mt-5" />
+      <h3 className="my-4 text-medium ">Total Credit Hour :{totalCredit} </h3>
+      <hr className="w-4/5 mx-auto" />
+      <h2 className="my-4 font-medium ">Total Price : {totalPrice} USD </h2>
 
-const Cart = () => {
-    return (
-        <div className="bg-white rounded-lg p-4">
-            <h2 className="text-[#2F80ED] font-bold mb-4 ">Credit Hour Remaining : </h2>
-            <hr className="w-4/5 mx-auto" />
-            <h2 className="my-4 font-bold text-xl ">Course Name</h2>
-        </div>
-    );
+    </div>
+  );
 };
 
+
+
+
+Cart.propTypes = {
+  selectedCourse: PropTypes.array,
+  totalPrice: PropTypes.number,
+  totalCredit:PropTypes.number,
+  creditRemaining: PropTypes.number
+}
 export default Cart;
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,15 +1,42 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import Cart from "./Cart/Cart";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Home = () => {
   const [courses, setCourses] = useState([]);
+  const [selectedCourse, setSelectedCourse] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0);
+  const [totalCredit, setTotalCredit] = useState(0);
+  const [creditRemaining, setCreditRemaining] = useState(0);
+
 
   useEffect(() => {
     fetch("./data.json")
       .then((res) => res.json())
       .then((data) => setCourses(data));
   }, []);
+
+  const handleCourse = (course) => {
+    const isExist = selectedCourse.find((item) => item.id === course.id);
+    let totalCost = course.price;
+    let credit = course.credit;
+    if (isExist) {
+      return toast.error("Course already purchased")
+    }
+    else{
+
+      selectedCourse.forEach(item => {
+        totalCost += item.price;
+        credit += item.credit
+      })
+      
+      setTotalPrice(totalCost);
+      setTotalCredit(credit)
+      setSelectedCourse([...selectedCourse, course]);
+    }
+  };
 
   return (
     <div className="flex w-11/12 mx-auto  gap-7">
@@ -28,10 +55,7 @@ const Home = () => {
                   viewBox="0 0 24 24"
                   fill="none"
                 >
-                  <path
-                    d="M12 1V23"
-                    stroke="#1C1B1B"
-                  />
+                  <path d="M12 1V23" stroke="#1C1B1B" />
                   <path
                     d="M17 5H9.5C8.57174 5 7.6815 5.36875 7.02513 6.02513C6.36875 6.6815 6 7.57174 6 8.5C6 9.42826 6.36875 10.3185 7.02513 10.9749C7.6815 11.6313 8.57174 12 9.5 12H14.5C15.4283 12 16.3185 12.3687 16.9749 13.0251C17.6313 13.6815 18 14.5717 18 15.5C18 16.4283 17.6313 17.3185 16.9749 17.9749C16.3185 18.6313 15.4283 19 14.5 19H6"
                     stroke="#1C1B1B"
@@ -55,14 +79,18 @@ const Home = () => {
                 Credit : {course.credit}h
               </p>
             </div>
-            <button className="bg-[#2F80ED] rounded-lg text-white w-full py-1">
+            <button
+              onClick={() => handleCourse(course)}
+              className="bg-[#2F80ED] rounded-lg text-white w-full py-1"
+            >
               Select
             </button>
           </div>
         ))}
       </div>
       <div className="w-1/4">
-        <Cart></Cart>
+        <Cart selectedCourse={selectedCourse} totalPrice = {totalPrice} totalCredit ={totalCredit} creditRemaining = {creditRemaining}></Cart>
+        <ToastContainer></ToastContainer>
       </div>
     </div>
   );
