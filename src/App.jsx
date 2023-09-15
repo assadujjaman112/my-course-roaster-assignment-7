23f715be5ac45c0a903c1cb01936a44cb081d433
assadujjaman112 <assadujjamanmurad120@gmail.com>
2023-09-15 20:35:01 +0600

    completed the display data on ui part

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 
+
 import './App.css'
+import Home from './Components/Home'
 
 function App() {
 
   return (
     <>
-      <h1>Course Registration</h1>
+      <h1 className='text-4xl text-center my-16 font-bold'>Course Registration</h1>
+      <Home></Home>
     </>
   )
 }
---
+++ src/Components/Cart/Cart.jsx
@@ -0,0 +1,13 @@
+
+
+const Cart = () => {
+    return (
+        <div className="bg-white rounded-lg p-4">
+            <h2 className="text-[#2F80ED] font-bold mb-4 ">Credit Hour Remaining : </h2>
+            <hr className="w-4/5 mx-auto" />
+            <h2 className="my-4 font-bold text-xl ">Course Name</h2>
+        </div>
+    );
+};
+
+export default Cart;
